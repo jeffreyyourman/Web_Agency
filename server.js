effcc54bad2475bc58907f1621065ea0ed40f40e
jeffreyyourman <jeffreyyourman@gmail.com>
2017-08-20 13:10:09 -0400

    saying techturtles isnt a valid login and its deff got the right login stuff

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ var express = require('express');
 var app = express();
 var nodemailer = require('nodemailer');
 
-
-
 // var methodOverride = require('method-override');
 // var request = require("request");
 
 //allow sessions
 // app.use(require("morgan")("combined"));
 // app.use(require("cookie-parser")());
-app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(require('body-parser').urlencoded({ extended: true }));
 // app.use(require("express-session")({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 //
 //Serve static content for the app from the "public" directory in the application directory.
@@ -18,48 +16,64 @@ app.use(express.static(process.cwd() + '/public'));
 // override with POST having ?_method=DELETE
 // app.use(methodOverride('_method'))
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
+app.engine(
+  'handlebars',
+  exphbs({
     defaultLayout: 'main'
-}));
+  })
+);
 app.set('view engine', 'handlebars');
 
-
-app.get('/', function(req,res){
+app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.post('/sendemail', function(req,res){
-  var name = req.body.name
-  var email = req.body.email
-  var number = req.body.number
-  var message = req.body.message
+app.post('/sendemail', function(req, res) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var number = req.body.number;
+  var message = req.body.message;
 
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: '@gmail.com',
+      user: 'hello@thetechturtles.com',
       pass: ''
+    },
+    dkim: {
+      domainName: 'www.thetechturtles.com',
+      keySelector: '2017',
+      privateKey: '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBg...'
     }
   });
 
   var mailOptions = {
-    from: "",
-    to: "",
+    from: '',
+    to: '',
     subject: name,
-    html: 'Name' + name + ' ' + '<br><br>' + 'Phone Number: ' + number + '<br><br>' + 'Anything else: ' + message
+    html:
+      'Name' +
+      name +
+      ' ' +
+      '<br><br>' +
+      'Phone Number: ' +
+      number +
+      '<br><br>' +
+      'Anything else: ' +
+      message
   };
 
-  transporter.sendMail(mailOptions, function(error, info){
+  transporter.sendMail(mailOptions, function(error, info) {
     if (error) {
       console.log(error);
     } else {
       console.log('Email sent: ' + info.response);
     }
   });
-  res.redirect('/')
+  res.redirect('/');
 });
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
   console.log('App listening on PORT: ' + PORT);
-})
+});
